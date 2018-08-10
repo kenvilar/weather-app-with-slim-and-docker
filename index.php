f92817ec78742d270222469c4afa13b048c2c01f
Ken Vilar <kenvilar@gmail.com>
2018-08-10 21:30:51 +0800

    Create a new container with guzzle injected

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 require "vendor/autoload.php";
 
+$container = new \Slim\Container([
+    'http' => function () {
+        return new GuzzleHttp\Client();
+    }
+]);
+
 // Instantiate the app object
-$app = new \Slim\App();
+$app = new \Slim\App($container);
 
 require "routes/web.php";
 
