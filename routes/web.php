41df7290da78680405ba0ee68ad268fcd4d11aa5
Ken Vilar <kenvilar@gmail.com>
2018-08-10 21:30:29 +0800

    Return the json result from third party weather api with specific ID

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 $app->get('/locations/{id}', function ($request, $response, $args) {
-    return $response->withStatus(200)->write("Location {$args['id']} retrieved.");
+    $result = $this->http->get("https://www.metaweather.com/api/location/{$args['id']}/")
+                         ->getBody()
+                         ->getContents();
+
+    return $response->withStatus(200)->withJson(json_decode($result));
 });
 
 $app->delete('/locations/{id}', function ($request, $response, $args) {
