334a30764742723b81a27026b5965437927044fc
Ken Vilar <kenvilar@gmail.com>
2018-08-10 23:20:19 +0800

    Get weather location id and if location found, then show it from DB otherwise query metaweather

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,28 @@
 <?php
 
 $app->get('/locations/{id}', function ($request, $response, $args) {
-    try {
-        $result = $this->http->get("https://www.metaweather.com/api/location/{$args['id']}/")
-                             ->getBody()
-                             ->getContents();
+    $id = $this->mysql->real_escape_string($args['id']);
+    $results = $this->mysql->query("SELECT * FROM locations WHERE id='{$id}'");
+    if ($results->num_rows > 0) {
+        $result = $results->fetch_assoc()['weather_app'];
+    } else {
+        try {
+            $result = $this->http->get("https://www.metaweather.com/api/location/{$args['id']}/")
+                ->getBody()
+                ->getContents();
 
-        return $response->withStatus(200)->withJson(json_decode($result));
-    } catch (Exception $e) {
-        print("Invalid ID");
-        exit();
+            $cleanResult = $this->mysql->real_escape_string($result);
+
+            if (!$this->mysql->query("INSERT INTO locations (id, weather) VALUES ('{$id}', '${cleanResult}')")) {
+                throw new Exception("Location could not be updated.");
+            }
+        } catch (Exception $e) {
+            print("Invalid ID");
+            exit();
+        }
     }
+
+    return $response->withStatus(200)->withJson(json_decode($result));
 });
 
 $app->delete('/locations/{id}', function ($request, $response, $args) {
